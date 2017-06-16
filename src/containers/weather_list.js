6b04ff93e9cd3c6b3642de3c3fe3b8322652407a
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-06-15 20:56:11 -0400

    Import react spark lines Create render weather function to return components with data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Sparklines, SparklinesLine} from 'react-sparklines'
 
 
 class WeatherList extends Component {
+
+  renderWeather(cityData){
+    const temps = cityData.list.map(weather => weather.main.temp)
+    return(
+      <tr key={cityData.city.name}>
+        <td>{cityData.city.name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red"/>
+          </Sparklines>
+        </td>
+      </tr>
+    )
+  }
+
   render(){
     return(
       <table className="table table-hover">
@@ -15,6 +31,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
@@ -26,4 +43,4 @@ function mapStateToProps({weather}){
   return {weather}
 }
 
-export default connect(mapStateToProps) (WeatherList)
+export default connect(mapStateToProps)(WeatherList)
